347b67d9c1535c740f7736442609af00b40b3fa1
osukhorukova <o.skhrkv@gmail.com>
2025-05-07 08:18:10 +0200

    Improved login.test.ts

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -2,23 +2,23 @@ import { test, expect } from '@playwright/test'
 import { existingUsers } from '../../test-setup/localstorage.setup'
 
 test.describe('login form tests', () => {
-  test('logging in works with existing account', async ({ page }) => {
-    await page.goto('/login')
+  existingUsers.forEach(user => {
+    test(`logging in works with ${user.firstName} ${user.lastName}`, async ({ page }) => {
+      await page.goto('/login')
 
-    const existingUser = existingUsers[0]
+      await page
+        .getByRole('textbox', { name: 'Email' })
+        .fill(user.email)
 
-    await page
-      .getByRole('textbox', { name: "Email" })
-      .fill(existingUser.email)
+      await page
+        .getByRole('textbox', { name: 'Password' })
+        .fill(user.password)
 
-    await page
-      .getByRole('textbox', { name: "Password" })
-      .fill(existingUser.password)
+      // Find and click the login button
+      await page.getByRole('button', { name: 'Login' }).click()
 
-    // Find and click the login button
-    await page.getByRole('button', { name: "Login" }).click()
-
-    // Verify successful login by checking for the "Log out" button
-    await expect(page.getByText('Log out')).toBeVisible()
+      // Verify successful login by checking for the "Log out" button
+      await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible()
+    })
   })
 })
